329e156d8a1b1154666dbb005ef128685c7b3dcf
Dakota-Han <hanjm1025@naver.com>
2022-09-20 19:51:56 +0900

    about page 09/20

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const main = require('./main');
-const user = require('./users')
+const user = require('./users');
+const testbed = require('./testbed');
+const about = require('./about')
 
 router.use('/main', main);
 router.use('/user', user);
-
+router.use('/testbed', testbed);
+router.use('/about',about);
 
 module.exports = router;
