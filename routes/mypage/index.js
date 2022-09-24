0aa130cff63810847f49df0a330055936dbedc5f
Dakota-Han <hanjm1025@naver.com>
2022-09-24 16:48:33 +0900

    /mypage/edit 추가 09/24

--- routes/mypage/index.js
+++ routes/mypage/index.js
@@ -4,10 +4,13 @@ var router = express.Router();
 
 
 /* GET home page. */
+router.get('/editcheck', function(req, res, next) {
+  res.render('editcheck');
+});
+
 router.get('/edit', function(req, res, next) {
   res.render('edit');
 });
-
 
 module.exports = router;
 
