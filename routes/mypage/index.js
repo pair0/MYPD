e3b9768419c47d8caf94ccf5062a3e93eb9dfce5
lego1404 <lego1404@korea.ac.kr>
2022-09-26 10:58:29 +0900

    test data upload page

--- routes/mypage/index.js
+++ routes/mypage/index.js
@@ -12,5 +12,18 @@ router.get('/edit', function(req, res, next) {
   res.render('edit');
 });
 
+router.get('/editdata', function(req, res, next) {
+  res.render('editdata');
+});
+
+router.get('/editmty', function(req, res, next) {
+  res.render('editmty');
+});
+
+/* 테스트 개발 페이지 (나중에 삭제) */
+router.get('/tmp', function(req, res, next) {
+  res.render('tmp');
+});
+
 module.exports = router;
 
