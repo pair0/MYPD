27a005f658eb397b01d6819426ceea154982a51a
Byounguk An <dbzl159753@naver.com>
2022-10-04 22:31:13 +0900

    1004

--- routes/testbed/index.js
+++ routes/testbed/index.js
@@ -1,25 +1,27 @@
-var express = require('express')
-var mdbConn = require('../../db_connection/mariaDBConn')
+var express = require("express");
+var mdbConn = require("../../db_connection/mariaDBConn");
 var router = express.Router();
-const { isLogIn }= require('../auth/auth')
+const { isLogIn } = require("../auth/auth");
 const { checkTokens } = require("../../passport/abouttoken");
 
 /* GET home page. */
-router.get('/', isLogIn, checkTokens, function(req, res, next) {
-  res.render('test');
+router.get("/", isLogIn, checkTokens, function (req, res, next) {
+  res.render("test");
 });
 
-router.get('/tmp', isLogIn, checkTokens, async function(req, res, next) {
-  console.log("와 여기"+req.user.id_idx);
-  var sql = 'SELECT * FROM service_test WHERE id_idx=?'
+router.get("/tmp", isLogIn, checkTokens, async function (req, res, next) {
+  console.log("와 여기" + req.user.id_idx);
+  var sql = "SELECT * FROM service_test WHERE id_idx=?";
   params = req.user.id_idx;
   var rows = await mdbConn.dbSelectall(sql, params);
-  console.log("asdf"+rows[0].service_id);
-  console.log("zxcv"+rows[1].service_id);
-  console.log("kkkkk"+rows.length);
   res.locals.service_select = rows;
-  res.render('tmp');
+  res.render("tmp");
 });
 
-
+router.post("/ServiceSelet", async function (req, res, next){
+  var data = req.body.data;
+  var sql = "SELECT * FROM service_test WHERE service_id=?";
+  var rows = await mdbConn.dbSelect(sql, data);
+  res.send(rows);
+});
 module.exports = router;
