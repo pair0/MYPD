06caa48730bb4f425f1851c25484e829b0c9f710
hyun-stone <hyun698207@gmail.com>
2022-10-04 20:53:15 +0900

    10/4 api + dbcheck

---
+++ routes/v1/pharmacy/pharmacy.js
@@ -0,0 +1,16 @@
+/**
+   * @path {GET} http://localhost:3000/v1/pharmacy/lists
+   * @description 의료기관약제내역목록 조회 API
+   */
+exports.lists = (req, res) => {
+    // res.set('x-api-tran-id', req.headers['x-api-tran-id'])
+    // res.json({ ok: true, code: "asdasdasdasdasd",state: req.query.state, api_tran_id: req.headers['x-api-tran-id']})
+}
+/**
+   * @path {POST} http://localhost:3000/v1/pharmacy/histories
+   * @description 의료기관약제내역 조회 API
+   */
+exports.histories = (req, res) => {
+    // res.set('x-api-tran-id', req.headers['x-api-tran-id'])
+    // res.json({ ok: true, code: "asdasdasdasdasd",state: req.query.state, api_tran_id: req.headers['x-api-tran-id']})
+}
