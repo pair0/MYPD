8a1821398e8378239fced23f075b52545071f1f3
hyeon_stone <hyun692807@gmail.com>
2022-12-14 19:37:33 +0900

    12/14 localhost 삭제

--- public/javascripts/CheckNumber.js
+++ public/javascripts/CheckNumber.js
@@ -8,7 +8,7 @@ $.ajax({
             console.log(data);
             if(!data){
                 var options = 'top=10, left=10, width=500, height=700, status=no, menubar=no, toolbar=no, resizable=no';
-                    window.open("https://localhost:3000/user/addinfo","EditInfo",options);
+                    window.open("https://mypd.kr/user/addinfo","EditInfo",options);
             }
         }
     });
